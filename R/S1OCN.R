5eb1f8b0998ae8fd33d7ca2cd4d5c2439ee84a0f
Juan Luis Herrera Cortijo <juan.luis.herrera.cortijo@gmail.com>
2024-09-02 05:49:26 +0000

    S1OC-9
    Implemented date filtering

--- R/S1OCN.R
+++ R/S1OCN.R
@@ -2,9 +2,9 @@ s1ocn_build_attribute_query <- function(attribute_name, attribute_value, value_o
 
   value_operator <- rlang::arg_match(value_operator,values = c("eq","le","ge","lt","gt"))
 
-attributes_list <- s1ocn_the$get_attributes_list()
+  attributes_list <- s1ocn_the$get_attributes_list()
 
-if(!attribute_name %in% attributes_list$Name) rlang::abort(glue::glue("{AttributeName} is not a valid attribute", AttributeName = attribute_name))
+  if(!attribute_name %in% attributes_list$Name) rlang::abort(glue::glue("{AttributeName} is not a valid attribute", AttributeName = attribute_name))
 
   value_type <-attributes_list %>% dplyr::filter(Name == attribute_name) %>% dplyr::pull("ValueType")
 
@@ -17,59 +17,98 @@ if(!attribute_name %in% attributes_list$Name) rlang::abort(glue::glue("{Attribut
                     AttributeName = attribute_name,
                     AttributeValue = attribute_value,
                     ValueOperator = value_operator
-                    )
+  )
 
   return(out)
 
 }
 
+s1ocn_parse_query_date <- function(x, default = lubridate::now(tzone = "UTC")) {
+
+  out <- rlang::try_fetch(
+    lubridate::ymd_hms(x,tz = "UTC"),
+    warning = function(cnd)
+      rlang::try_fetch(
+        lubridate::ymd_hm(x,tz = "UTC"),
+        warning = function(cnd)
+          rlang::try_fetch(
+            lubridate::ymd_h(x,tz = "UTC"),
+            warning = function(cnd)
+              rlang::try_fetch(
+                lubridate::ymd(x,tz = "UTC"),
+                warning = function(cnd)
+                  default)
+          )
+      )
+  )
+  if(lubridate::is.timepoint(out)){
+    out <- format(out, "%Y-%m-%dT%H:%M:%S.000Z")
+  }
+  return(out)
+}
+
 #' @param max_results The default value is set to 20.The acceptable arguments for this option: Integer <0,1000>.https://documentation.dataspace.copernicus.eu/APIs/OData.html#top-option
 #' @param attributes_list swathIdentifier is one of Aquisition modes here https://sentiwiki.copernicus.eu/web/s1-products#S1Products-Level-2ProductsS1-Products-Level-2-Products
-s1ocn_list_files <-function(max_results = 20, attributes_search = list()){
+#' @examples
+#' s1ocn_list_files(attributes_search = list("swathIdentifier"="IW", orbitDirection = "DESCENDING", relativeOrbitNumber = 125))
+#' s1ocn_list_files(attributes_search = list("swathIdentifier"="IW", orbitDirection = "DESCENDING", relativeOrbitNumber = 125), datetime_start = "2021-01-01")
+s1ocn_list_files <-function(datetime_start = rlang::zap(), datetime_end = rlang::zap(), max_results = 20, attributes_search = list()){
 
   iterate_pages <- max_results > 1000
 
 
 
 
-  url <- glue::glue("https://catalogue.dataspace.copernicus.eu/odata/v1/Products?$top={MaxResults}&$filter=Collection/Name%20eq%20%27SENTINEL-1%27", MaxResults = max_results)
+  url <- glue::glue("https://catalogue.dataspace.copernicus.eu/odata/v1/Products?$orderby=ContentDate/Start%20asc&$top={MaxResults}&$filter=Collection/Name%20eq%20%27SENTINEL-1%27", MaxResults = max_results)
 
   product_type_query <- s1ocn_build_attribute_query("productType","OCN") #https://sentiwiki.copernicus.eu/web/s1-products#S1Products-Level-2ProductsS1-Products-Level-2-Products
 
-url <- glue::glue("{url}%20and%20{product_type_query}")
+  url <- glue::glue("{url}%20and%20{product_type_query}")
 
-attributes_search$productType <- NULL
+  attributes_search$productType <- NULL
 
-if(length(attributes_search) > 0){
-  url <- purrr::reduce2(attributes_search, names(attributes_search),
-                        \(url_so_far,attribute_value, attribute_name){
+  if(length(attributes_search) > 0){
+    url <- purrr::reduce2(attributes_search, names(attributes_search),
+                          \(url_so_far,attribute_value, attribute_name){
 
-                          attribute_query <- s1ocn_build_attribute_query(attribute_name, attribute_value, value_operator = "eq")
+                            attribute_query <- s1ocn_build_attribute_query(attribute_name, attribute_value, value_operator = "eq")
 
-                          glue::glue("{url_so_far}%20and%20{attribute_query}")
+                            glue::glue("{url_so_far}%20and%20{attribute_query}")
 
-                        }, .init = url)
-}
+                          }, .init = url)
+  }
 
-url <- as.character(url)
+
+  if(any(!rlang::is_zap(c(datetime_start, datetime_end)))){
+
+    datetime_start <- if(rlang::is_zap(datetime_start)) "1900-01-01T00:00:00.000Z" else s1ocn_parse_query_date(datetime_start)
+
+    datetime_end <- if(rlang::is_zap(datetime_end)) s1ocn_parse_query_date(lubridate::now(tzone = "UTC")) else s1ocn_parse_query_date(datetime_end)
+
+    url <- glue::glue("{url}%20and%20ContentDate/Start%20ge%20{StartPeriod}%20and%20ContentDate/Start%20le%20{EndPeriod}", StartPeriod = datetime_start, EndPeriod = datetime_end)
+
+  }
+
+  url <- as.character(url)
 
   con <- curl::curl(url)
-  response <- suppressWarnings(readLines(con)) %>% jsonlite::fromJSON()
 
-out <- response$value
+  response <-suppressWarnings(readLines(con)) %>% jsonlite::fromJSON()
 
-while(iterate_pages && !is.null(response$`@odata.nextLink`)){
+  out <- response$value
 
-  url <- as.character(response$`@odata.nextLink`)
+  while(iterate_pages && !is.null(response$`@odata.nextLink`)){
 
-  con <- curl::curl(url)
-  response <- suppressWarnings(readLines(con)) %>% jsonlite::fromJSON()
+    url <- as.character(response$`@odata.nextLink`)
 
-  out %<>% dplyr::bind_rows(response$value)
+    con <- curl::curl(url)
+    response <- suppressWarnings(readLines(con)) %>% jsonlite::fromJSON()
 
-}
+    out %<>% dplyr::bind_rows(response$value)
+
+  }
 
 
-return(out)
+  return(out)
 
 }
