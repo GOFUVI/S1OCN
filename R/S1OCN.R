91c0d113f77d4a9d0a91450cec5172e4f292c54f
Juan Luis Herrera Cortijo <juan.luis.herrera.cortijo@gmail.com>
2024-09-03 06:21:03 +0200

    S1OC-8 (#2)
    geographical filtering

--- R/S1OCN.R
+++ R/S1OCN.R
@@ -47,12 +47,55 @@ s1ocn_parse_query_date <- function(x, default = lubridate::now(tzone = "UTC")) {
   return(out)
 }
 
+s1ocn_parse_search_polygon <- function(search_polygon){
+
+  default_search_polygon <- sf::st_polygon(list(matrix(c(-90,-180,-90,180,90,180,90,-180,-90,-180), ncol = 2, byrow = T)))
+
+  if(!rlang::inherits_any(search_polygon, c("POLYGON"))){
+
+    if(rlang::inherits_any(search_polygon, "data.frame")){
+
+      search_polygon <- as.matrix(search_polygon)
+    }
+
+    if(is.matrix(search_polygon)){
+      search_polygon <- rlang::try_fetch(sf::st_polygon(list(search_polygon)),
+                                         error = function(cnd){
+                                           rlang::warn("Search polygon conversion failed. Performing worldwide search.", parent = cnd)
+                                           default_search_polygon
+                                         })
+    }
+
+    if(rlang::inherits_any(search_polygon, "character")){
+      search_polygon <- rlang::try_fetch(sf::st_as_sfc(search_polygon),
+                                         error = function(cnd){
+                                           rlang::warn("Search polygon conversion failed. Performing worldwide search.", parent = cnd)
+                                           default_search_polygon
+                                         })
+    }
+
+  }
+
+  if(!sf::st_is_valid(search_polygon) || !sf::st_is(search_polygon, "POLYGON")){
+    search_polygon <- default_search_polygon
+  }
+
+  search_polygon %<>% sf::st_as_text()
+
+  search_polygon %<>% stringr::str_replace_all("\\s","%20")
+
+
+  return(search_polygon)
+
+}
+
 #' @param max_results The default value is set to 20.The acceptable arguments for this option: Integer <0,1000>.https://documentation.dataspace.copernicus.eu/APIs/OData.html#top-option
 #' @param attributes_list swathIdentifier is one of Aquisition modes here https://sentiwiki.copernicus.eu/web/s1-products#S1Products-Level-2ProductsS1-Products-Level-2-Products
 #' @examples
 #' s1ocn_list_files(attributes_search = list("swathIdentifier"="IW", orbitDirection = "DESCENDING", relativeOrbitNumber = 125))
 #' s1ocn_list_files(attributes_search = list("swathIdentifier"="IW", orbitDirection = "DESCENDING", relativeOrbitNumber = 125), datetime_start = "2021-01-01")
-s1ocn_list_files <-function(datetime_start = rlang::zap(), datetime_end = rlang::zap(), max_results = 20, attributes_search = list()){
+#' s1ocn_list_files(attributes_search = list("swathIdentifier"="IW", orbitDirection = "DESCENDING", relativeOrbitNumber = 125), datetime_start = "2021-01-01", search_polygon = matrix(c(-11, 41, -11, 44, 6, 44, 6, 41, -11, 41), ncol = 2, byrow = T))
+s1ocn_list_files <-function(search_polygon = rlang::zap(), datetime_start = rlang::zap(), datetime_end = rlang::zap(), max_results = 20, attributes_search = list()){
 
   iterate_pages <- max_results > 1000
 
@@ -86,6 +129,15 @@ s1ocn_list_files <-function(datetime_start = rlang::zap(), datetime_end = rlang:
     datetime_end <- if(rlang::is_zap(datetime_end)) s1ocn_parse_query_date(lubridate::now(tzone = "UTC")) else s1ocn_parse_query_date(datetime_end)
 
     url <- glue::glue("{url}%20and%20ContentDate/Start%20ge%20{StartPeriod}%20and%20ContentDate/Start%20le%20{EndPeriod}", StartPeriod = datetime_start, EndPeriod = datetime_end)
+
+  }
+
+  if(!rlang::is_zap(search_polygon)){
+
+search_polygon %<>% s1ocn_parse_search_polygon()
+
+    search_polygon_query  <- glue::glue("OData.CSC.Intersects(area=geography%27SRID=4326;{SearchPolygon}%27)", SearchPolygon = search_polygon)
+    url <- glue::glue("{url}%20and%20{search_polygon_query}")
 
   }
 
